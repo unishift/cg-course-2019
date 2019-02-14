c6bf65b98dacb1cb8fefd7afc3f5642def005d67
unishift <unishifft@gmail.com>
2019-02-14 13:27:33 +0300

    Reformat code

--- ShaderProgram.h
+++ ShaderProgram.h
@@ -6,43 +6,40 @@
 
 #include "LiteMath.h"
 
-class ShaderProgram
-{
+class ShaderProgram {
 public:
 
-  ShaderProgram() : shaderProgram(-1) {};
+    ShaderProgram() : shaderProgram(-1) {};
 
-  ShaderProgram(const std::unordered_map<GLenum, std::string> &inputShaders);
+    ShaderProgram(const std::unordered_map<GLenum, std::string> &inputShaders);
 
-  virtual ~ShaderProgram() {};
+    virtual ~ShaderProgram() {};
 
-  void Release(); //actual destructor
+    void Release(); //actual destructor
 
-  virtual void StartUseShader() const;
+    virtual void StartUseShader() const;
 
-  virtual void StopUseShader() const;
+    virtual void StopUseShader() const;
 
-  GLuint GetProgram() const { return shaderProgram; }
+    GLuint GetProgram() const { return shaderProgram; }
 
+    bool reLink();
 
-  bool reLink();
+    void SetUniform(const std::string &location, float value) const;
 
-  void SetUniform(const std::string &location, float value) const;
+    void SetUniform(const std::string &location, double value) const;
 
-  void SetUniform(const std::string &location, double value) const;
+    void SetUniform(const std::string &location, int value) const;
 
-  void SetUniform(const std::string &location, int value) const;
+    void SetUniform(const std::string &location, unsigned int value) const;
 
-  void SetUniform(const std::string &location, unsigned int value) const;
-
-  void SetUniform(const std::string &location, LiteMath::float4x4) const;
+    void SetUniform(const std::string &location, LiteMath::float4x4) const;
 
 private:
-  static GLuint LoadShaderObject(GLenum type, const std::string &filename);
+    static GLuint LoadShaderObject(GLenum type, const std::string &filename);
 
-  GLuint shaderProgram;
-  std::unordered_map<GLenum, GLuint> shaderObjects;
+    GLuint shaderProgram;
+    std::unordered_map<GLenum, GLuint> shaderObjects;
 };
-
 
 #endif
--- common.h
+++ common.h
@@ -1,7 +1,6 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -19,55 +18,44 @@
 //#define PI 3.1415926535897932384626433832795f
 
 
-static void ThrowExceptionOnGLError(int line, const char *file)
-{
+static void ThrowExceptionOnGLError(int line, const char *file) {
 
-	static char errMsg[512];
+    static char errMsg[512];
 
-	//вызывается функция glGetError, проверяющая не произошла ли ошибка
-	//в каком-то вызове opengl и если произошла, то какой код у ошибки
-	GLenum gl_error = glGetError();
+    //вызывается функция glGetError, проверяющая не произошла ли ошибка
+    //в каком-то вызове opengl и если произошла, то какой код у ошибки
+    GLenum gl_error = glGetError();
 
-	if (gl_error == GL_NO_ERROR)
-		return;
+    if (gl_error == GL_NO_ERROR)
+        return;
 
-	switch (gl_error)
-	{
-	case GL_INVALID_ENUM:
-		std::cerr << "GL_INVALID_ENUM file " << file << " line " << line << std::endl;
-		break;
+    switch (gl_error) {
+        case GL_INVALID_ENUM: std::cerr << "GL_INVALID_ENUM file " << file << " line " << line << std::endl;
+            break;
 
-	case GL_INVALID_VALUE:
-		std::cerr << "GL_INVALID_VALUE file " << file << " line " << line << std::endl;
-		break;
+        case GL_INVALID_VALUE: std::cerr << "GL_INVALID_VALUE file " << file << " line " << line << std::endl;
+            break;
 
-	case GL_INVALID_OPERATION:
-		std::cerr << "GL_INVALID_OPERATION file " << file << " line " << line << std::endl;
-		break;
+        case GL_INVALID_OPERATION: std::cerr << "GL_INVALID_OPERATION file " << file << " line " << line << std::endl;
+            break;
 
-	case GL_STACK_OVERFLOW:
-		std::cerr << "GL_STACK_OVERFLOW file " << file << " line " << line << std::endl;
-		break;
+        case GL_STACK_OVERFLOW: std::cerr << "GL_STACK_OVERFLOW file " << file << " line " << line << std::endl;
+            break;
 
-	case GL_STACK_UNDERFLOW:
-		std::cerr << "GL_STACK_UNDERFLOW file " << file << " line " << line << std::endl;
-		break;
+        case GL_STACK_UNDERFLOW: std::cerr << "GL_STACK_UNDERFLOW file " << file << " line " << line << std::endl;
+            break;
 
-	case GL_OUT_OF_MEMORY:
-		std::cerr << "GL_OUT_OF_MEMORY file " << file << " line " << line << std::endl;
-		break;
+        case GL_OUT_OF_MEMORY: std::cerr << "GL_OUT_OF_MEMORY file " << file << " line " << line << std::endl;
+            break;
 
-	case GL_NO_ERROR:
-		break;
+        case GL_NO_ERROR: break;
 
-	default:
-		std::cerr << "Unknown error @ file " << file << " line " << line << std::endl;
-		break;
-	}
+        default: std::cerr << "Unknown error @ file " << file << " line " << line << std::endl;
+            break;
+    }
 
-	if (gl_error != GL_NO_ERROR)
-		throw std::runtime_error(errMsg);
+    if (gl_error != GL_NO_ERROR)
+        throw std::runtime_error(errMsg);
 }
-
 
 #endif

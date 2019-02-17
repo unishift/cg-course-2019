55942eb80fbd9460942beb7b5b4715fe71865a7d
unishift <unishifft@gmail.com>
2019-02-17 18:23:06 +0300

    Add second light source to scene

--- shaders/raytrace.glsl
+++ shaders/raytrace.glsl
@@ -58,6 +58,10 @@ const LightSource lights[] = LightSource[](
     LightSource(
         float3(0.0, -4.0, 10.0),
         1.0
+    ),
+    LightSource(
+        float3(0.0, 15.0, -5.0),
+        0.5
     )
 );
 

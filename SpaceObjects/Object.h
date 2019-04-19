e725f94ac606156422d213247c946ff23edfb1f7
unishift <unishifft@gmail.com>
2019-04-19 13:42:25 +0300

    Move Object constructor to .cpp file

--- SpaceObjects/Object.h
+++ SpaceObjects/Object.h
@@ -15,29 +15,7 @@ public:
     glm::vec3 world_pos;
     glm::mat4 rot;
 
-    Object(const std::vector<float>& vertices, const std::vector<GLuint>& elements) :
-        vertices(vertices),
-        elements(elements),
-        world_pos(0.0f, 0.0f, 0.0f),
-        rot(1.0f) {
-
-        glGenVertexArrays(1, &VAO);
-        glGenBuffers(1, &VBO);
-        glGenBuffers(1, &EBO);
-
-        glBindVertexArray(VAO);
-
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
-
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elements.size() * sizeof(GLuint), elements.data(), GL_STATIC_DRAW);
-
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLuint), nullptr);
-
-        glBindVertexArray(0);
-    }
+    Object(const std::vector<float>& vertices, const std::vector<GLuint>& elements);
 
     void move(const glm::vec3& translation) {
         world_pos += translation;

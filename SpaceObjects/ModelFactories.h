fa078346744640356a296c985963e29447741f65
unishift <unishifft@gmail.com>
2019-05-05 02:27:04 +0300

    Optimize loading of models

--- SpaceObjects/ModelFactories.h
+++ SpaceObjects/ModelFactories.h
@@ -3,6 +3,7 @@
 
 #include "Model.h"
 
+#include <map>
 #include <glm/gtx/norm.hpp>
 
 enum class ModelName {
@@ -11,24 +12,30 @@ enum class ModelName {
     REPVENATOR,
 };
 
+std::map<ModelName, Model> model_buffer;
+
 static inline
 Model create_model(ModelName model_name, const glm::vec3& position = glm::vec3(0.0f), const glm::vec3& rotation = glm::vec3(0.0f), float scale = 1.0f) {
     Model model;
 
-    switch (model_name) {
-        case ModelName::E45_AIRCRAFT:
-            model =  Model("models/E-45-Aircraft/E 45 Aircraft_obj.obj");
-            model.rotate(M_PI, {0, 1, 0});
-            model.move({0, 0, 3});
-            break;
-        case ModelName::ENTERPRISE_NCC1701D:
-            model =  Model("models/Enterprise NCC 1701 D/enterprise1701d.obj");
-            model.move({-44.15, -4, 8});
-            break;
-        case ModelName::REPVENATOR:
-            model =  Model("models/Venator/export.obj");
-            break;
-
+    if (model_buffer.count(model_name) == 0) {
+        switch (model_name) {
+            case ModelName::E45_AIRCRAFT:
+                model = Model("models/E-45-Aircraft/E 45 Aircraft_obj.obj");
+                model.rotate(M_PI, {0, 1, 0});
+                model.move({0, 0, 3});
+                break;
+            case ModelName::ENTERPRISE_NCC1701D:
+                model = Model("models/Enterprise NCC 1701 D/enterprise1701d.obj");
+                model.move({-44.15, -4, 8});
+                break;
+            case ModelName::REPVENATOR:
+                model = Model("models/Venator/export.obj");
+                break;
+        }
+        model_buffer[model_name] = model;
+    } else {
+        model = model_buffer[model_name];
     }
 
     model.scale(scale);

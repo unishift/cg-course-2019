f1cbec887c2be90f7ca7d845cefd9b4b619c8be9
unishift <unishifft@gmail.com>
2019-05-04 13:10:13 +0300

    Enable anti-aliasing

--- SpaceObjects/main.cpp
+++ SpaceObjects/main.cpp
@@ -170,6 +170,7 @@ int main(int argc, char **argv) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
 
     GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL basic sample", nullptr, nullptr);
     if (window == nullptr) {
@@ -235,6 +236,7 @@ int main(int argc, char **argv) {
     glm::vec3 smooth_step(0.0f);
     float smooth_rot_step = 0.0f;
 
+    glEnable(GL_MULTISAMPLE);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);

5d4879ef113e99c79f7b3e2f325cc73a61b31623
unishift <unishifft@gmail.com>
2019-05-10 20:44:10 +0300

    Move enemy spawn further

--- SpaceObjects/ModelFactories.h
+++ SpaceObjects/ModelFactories.h
@@ -24,9 +24,9 @@ public:
     Model get_model(ModelName model_name, const glm::vec3& position = glm::vec3(0.0f), const glm::vec3& rotation = glm::vec3(0.0f), float scale = 1.0f) const;
 
     Model get_random_enemy(const glm::vec3& position) const {
-        const float x = rand() % 50 - 25;
+        const float x = rand() % 100 - 50;
         const float y = rand() % 50 - 25;
-        const auto src = glm::vec3(x, y, -200.0f) + position;
+        const auto src = glm::vec3(x, y, -500.0f) + position;
 
         const auto choice = static_cast<ModelName>(ModelName::REPVENATOR + rand() % 3);
 
@@ -34,10 +34,10 @@ public:
     }
 
     Asteroid get_random_asteroid(const glm::vec3& position, const glm::vec3& target) const {
-        const float x = rand() % 50 - 25;
+        const float x = rand() % 100 - 50;
         const float y = rand() % 50 - 25;
         const float scale = (rand() % 50) / 50.0f + 0.5f;
-        const auto src = glm::vec3(x, y, -200.0f) + position;
+        const auto src = glm::vec3(x, y, -500.0f) + position;
         const glm::vec3 velocity = glm::normalize(target - src);
 
         const auto choice = static_cast<ModelName>(ModelName::MYST_ASTEROID + rand() % 2);
